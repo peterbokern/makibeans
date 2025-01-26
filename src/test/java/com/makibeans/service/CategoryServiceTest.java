f82852dd8cad99192b9fe060269430b762182577
peterbokern <peterbokern@gmail.com>
2025-01-26 16:48:56 +0100

    Added unit test testCreateRootCategoryWithValidName.

--- src/test/java/com/makibeans/service/CategoryServiceTest.java
+++ src/test/java/com/makibeans/service/CategoryServiceTest.java
@@ -1,11 +1,66 @@
 package com.makibeans.service;
 
+import com.makibeans.model.Category;
+import com.makibeans.repository.CategoryRepository;
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
+import com.makibeans.exeptions.DuplicateResourceException;
+import com.makibeans.exeptions.ResourceNotFoundException;
+import com.makibeans.model.AttributeTemplate;
+import com.makibeans.model.AttributeValue;
+import com.makibeans.repository.AttributeValueRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.openMocks;
 
 class CategoryServiceTest {
 
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    @InjectMocks
+    private CategoryService categoryService;
+
+    @BeforeEach
+    void setUp() {
+        openMocks(this);
+    }
+
+    //Create root category tests
+    @Test
+    void testCreateRootCategoryWithValidName() {
+
+        //arrange
+        when(categoryRepository.existsByNameAndParentCategory("Coffee", null)).thenReturn(false);
+        when(categoryRepository.save(any(Category.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        //act
+        Category result = categoryService.createRootCategory("Coffee", "Description", "imageUrl");
+
+        //assert
+        assertNotNull(result, "The result should not be null");
+        assertEquals("Coffee", result.getName(), "Category name mismatch");
+        assertNull(result.getParentCategory(), "Parent category should be null for a root category");
+        assertEquals("Description", result.getDescription(), "Category description mismatch");
+        assertEquals("imageUrl", result.getImageUrl(), "Image URL mismatch");
+        verify(categoryRepository).existsByNameAndParentCategory(eq("Coffee"), eq(null));
+        verify(categoryRepository).save(any(Category.class));
+    }
+
   /*  1. createRootCategory
-    testCreateRootCategoryWithValidName()
+
     testCreateRootCategoryWithNullOrEmptyName()
     testCreateDuplicateRootCategory()
 
