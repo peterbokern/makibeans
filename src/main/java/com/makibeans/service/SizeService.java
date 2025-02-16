d8000f8f2aa5e1664f17b7eeaba581baf5ed3f5b
peterbokern <peterbokern@gmail.com>
2025-02-16 17:07:18 +0100

    Created service class for Size.

--- src/main/java/com/makibeans/service/SizeService.java
+++ src/main/java/com/makibeans/service/SizeService.java
@@ -6,6 +6,7 @@ import com.makibeans.repository.SizeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class SizeService extends AbstractCrudService<Size, Long> {
@@ -18,6 +19,15 @@ public class SizeService extends AbstractCrudService<Size, Long> {
         this.sizeRepository = sizeRepository;
     }
 
+    /**
+     * Creates a new Size entity.
+     *
+     * @param name The name of the size.
+     * @return The saved Size entity.
+     * @throws DuplicateResourceException If a size with the same name already exists.
+     */
+
+    @Transactional
     public Size createSize(String name) {
 
         if (sizeRepository.existsByName(name)) {
@@ -26,10 +36,39 @@ public class SizeService extends AbstractCrudService<Size, Long> {
 
         Size size = new Size(name);
 
-        return sizeRepository.save(size);
+        return create(size);
     }
 
-    public void deleteSize(Long sizeId){
+    /**
+     * Deletes a Size by ID.
+     *
+     * @param sizeId The ID of the size to delete.
+     */
 
+    @Transactional
+    public void deleteSize(Long sizeId){
+        delete(sizeId);
+    }
+
+    /**
+     * Updates an existing Size.
+     *
+     * @param sizeId      The ID of the size to update.
+     * @param updatedName The new name for the size.
+     * @return The updated Size entity.
+     * @throws DuplicateResourceException If a size with the same name already exists.
+     */
+
+    @Transactional
+    public Size updateSize(Long sizeId, String updatedName) {
+        Size size = findById(sizeId);
+
+        if (!size.getName().equals(updatedName) && sizeRepository.existsByName(updatedName)) {
+            throw new DuplicateResourceException("Size with name '" + updatedName + "' already exists.");
+        }
+
+        size.setName(updatedName);
+
+        return update(sizeId, size);
     }
 }
