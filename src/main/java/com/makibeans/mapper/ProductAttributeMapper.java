ed2cad67e87588cdd29af1d9e7b8a82b3aeb35f0
peterbokern <peterbokern@gmail.com>
2025-03-01 15:24:35 +0100

    Created product attribute mapper.

---
+++ src/main/java/com/makibeans/mapper/ProductAttributeMapper.java
@@ -0,0 +1,16 @@
+package com.makibeans.mapper;
+
+import com.makibeans.dto.ProductAttributeResponseDTO;
+import com.makibeans.model.ProductAttribute;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring", uses = AttributeValueMapper.class)
+public interface ProductAttributeMapper {
+
+    @Mapping(source = "product.id", target = "productId")
+    @Mapping(source = "product.name", target = "productName")
+    @Mapping(source = "attributeTemplate.id", target = "attributeTemplateId")
+    @Mapping(source = "attributeTemplate.name", target = "attributeTemplateName")
+    @Mapping(source = "attributeValues", target = "attributeValues")
+    ProductAttributeResponseDTO toResponseDTO(ProductAttribute entity);
+}
